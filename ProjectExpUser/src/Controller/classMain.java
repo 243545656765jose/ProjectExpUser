5db372cb1e61501d51fadf28381a763eefcac772
Jose Andres <joseandresacuna087@gmail.com>
2023-11-03 14:33:02 -0600

    Add package interface and class main

---
+++ ProjectExpUser/src/Controller/classMain.java
@@ -0,0 +1,16 @@
+
+package Controller;
+
+import View.Login;
+
+
+public class classMain {
+
+
+    public static void main(String[] args) {
+       Login login = new Login();
+       login.setVisible(true);
+       login.setLocationRelativeTo(null);
+    }
+    
+}
